87daa16da1b171cb197374d95552526edaa3c0ef
Dmitry Sharsniou <shersnevbm@gmail.com>
2026-01-30 15:55:21 +0300

    new commit

--- src/Task.jsx
+++ src/Task.jsx
@@ -3,19 +3,17 @@ import { useState, useRef, useEffect } from "react";
 const Task = ({ item, deleteTask, changeCheckbox, changeTitle }) => {
   const [isEdit, setIsEdit] = useState(false);
   const [editText, setEditText] = useState(item.title);
+  const [error, setError] = useState(false);
   const inputRef = useRef(null);
 
   useEffect(() => {
     if (!isEdit) return;
-
     const handleClickOutside = (event) => {
       if (inputRef.current && !inputRef.current.contains(event.target)) {
         setIsEdit(false);
       }
     };
-
     document.addEventListener("mousedown", handleClickOutside);
-
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
@@ -23,44 +21,63 @@ const Task = ({ item, deleteTask, changeCheckbox, changeTitle }) => {
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      changeTitle(item.id, editText);
-      setIsEdit(false);
-    } else if (e.key === "Escape") {
+      if (editText.trim() !== "") {
+        changeTitle(item.id, editText);
+        setIsEdit(false);
+        setError(false);
+      } else {
+        setError(true);
+      }
+    }
+    if (e.key === "Escape") {
       setIsEdit(false);
     }
   };
 
-  const handleSaveClick = () => {
-    changeTitle(item.id, editText);
-    setIsEdit(false);
+  const handleClick = () => {
+    if (editText.trim() !== "") {
+      changeTitle(item.id, editText);
+      setIsEdit(false);
+      setError(false);
+    } else {
+      setError(true);
+    }
   };
 
   return (
-    <div className="task">
-      <input
-        type="checkbox"
-        checked={item.isDone}
-        onChange={() => changeCheckbox(item.id)}
-      />
-      {isEdit ? (
-        <>
-          <input
-            ref={inputRef}
-            value={editText}
-            onChange={(e) => setEditText(e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-          <button onClick={handleSaveClick}>💾</button>
-        </>
-      ) : (
-        <>
-          <p className={item.isDone ? "checked" : ""}>{item.title}</p>
-          <button onClick={() => setIsEdit(true)}>🖊️</button>
-        </>
-      )}
+    <>
+      <div className="task">
+        <input
+          type="checkbox"
+          checked={item.isDone}
+          onChange={() => changeCheckbox(item.id)}
+        />
+        {isEdit ? (
+          <>
+            <div ref={inputRef}>
+              <input
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />{" "}
+              <button onClick={handleClick}>💾</button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className={item.isDone ? "checked" : ""}>{item.title}</p>
+            <button onClick={() => setIsEdit(true)}>🖊️</button>
+          </>
+        )}
 
-      <button onClick={() => deleteTask(item.id)}>❌</button>
-    </div>
+        <button onClick={() => deleteTask(item.id)}>❌</button>
+      </div>
+      {error && (
+        <p style={{ color: "red" }}>
+          Строка не должна быть пустой или состоять только из пробелов
+        </p>
+      )}
+    </>
   );
 };
 
